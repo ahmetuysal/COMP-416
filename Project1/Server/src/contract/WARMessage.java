efda49d22313cc78347f4ee0c97fc8e1ccafc274
Ahmet Uysal <auysal16@ku.edu.tr>
2020-02-17 23:53:43 +0300

    Create WAR Message class

---
+++ Project1/Server/src/contract/WARMessage.java
@@ -0,0 +1,30 @@
+package contract;
+
+import java.io.Serializable;
+
+public class WARMessage implements Serializable {
+    private static final long serialVersionUID = -5817893373429527297L;
+    private byte type;
+    private byte[] payload;
+
+    public WARMessage(byte type, byte[] payload) {
+        this.type = type;
+        this.payload = payload;
+    }
+
+    public byte getType() {
+        return type;
+    }
+
+    public void setType(byte type) {
+        this.type = type;
+    }
+
+    public byte[] getPayload() {
+        return payload;
+    }
+
+    public void setPayload(byte[] payload) {
+        this.payload = payload;
+    }
+}
