d9c8dde5cc9b399fdd084636ecb8dd64eacecf80
Ahmet Uysal <auysal16@ku.edu.tr>
2020-02-18 12:30:57 +0300

    Implement server code that is responsible for establishing connections and creating controller threads

--- Project1/Server/src/Main.java
+++ Project1/Server/src/Main.java
@@ -1,7 +1,9 @@
+/**
+ * @author Ahmet Uysal @ahmetuysal, Ipek Koprululu @ipekkoprululu, Furkan Sahbaz @fsahbaz
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello, world!");
+        new Server(Server.DEFAULT_SERVER_PORT);
     }
 }
---
+++ Project1/Server/src/Server.java
@@ -0,0 +1,52 @@
+import controller.ControllerThread;
+
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author Ahmet Uysal @ahmetuysal, Ipek Koprululu @ipekkoprululu, Furkan Sahbaz @fsahbaz
+ */
+public class Server {
+    public static final int DEFAULT_SERVER_PORT = 4444;
+    private ServerSocket serverSocket;
+
+    /**
+     * Initiates a server socket on the input port, listens to the line, on receiving an incoming
+     * connection creates and starts a ServerThread on the client
+     *
+     * @param port port to open a socket on
+     */
+    public Server(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Opened up a server socket on " + Inet4Address.getLocalHost());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Server class.Constructor exception on opening a server socket");
+        }
+        while (true) {
+            ListenAndAccept();
+        }
+    }
+
+    /**
+     * Listens to the line and starts a connection on receiving a request from the client
+     * The connection is started and initiated as a ServerThread object
+     */
+    private void ListenAndAccept() {
+        Socket socket;
+        try {
+            socket = serverSocket.accept();
+            System.out.println("A connection was established with a client on the address of " + socket.getRemoteSocketAddress());
+            ControllerThread controllerThread = new ControllerThread(socket);
+            controllerThread.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Server Class.Connection establishment error inside listen and accept function");
+        }
+    }
+
+}
+
