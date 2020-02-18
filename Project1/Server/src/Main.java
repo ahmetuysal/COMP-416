65ea43ae48fb2de23b2325c581a9c4e5cceead89
Ahmet Uysal <auysal16@ku.edu.tr>
2020-02-18 18:19:15 +0300

    Remove *.jar from gitignore

--- Project1/Server/src/Main.java
+++ Project1/Server/src/Main.java
@@ -1,9 +1,12 @@
+import repository.MongoDBWARRepository;
+
 /**
  * @author Ahmet Uysal @ahmetuysal, Ipek Koprululu @ipekkoprululu, Furkan Sahbaz @fsahbaz
  */
 public class Main {
 
     public static void main(String[] args) {
+        MongoDBWARRepository.getInstance() ;
         new Server(Server.DEFAULT_SERVER_PORT);
     }
 }
--- Project1/Server/src/repository/MongoDBWARRepository.java
+++ Project1/Server/src/repository/MongoDBWARRepository.java
@@ -7,9 +7,28 @@ import com.mongodb.ConnectionString;
 import com.mongodb.ServerAddress;
 import com.mongodb.MongoCredential;
 import com.mongodb.MongoClientOptions;
+import com.mongodb.client.MongoDatabase;
 
 /**
  * @author Ahmet Uysal @ahmetuysal, Ipek Koprululu @ikoprululu, Furkan Sahbaz @fsahbaz
  */
 public class MongoDBWARRepository implements WARRepository {
+
+    private static MongoDBWARRepository _instance;
+
+    public static synchronized MongoDBWARRepository getInstance() {
+        if (_instance == null) {
+            _instance = new MongoDBWARRepository();
+        }
+        return _instance;
+    }
+
+    private MongoClient mongoClient;
+    private MongoDatabase WARDatabase;
+
+    private MongoDBWARRepository() {
+        // by default, this will connect to localhost:27017
+        mongoClient = MongoClients.create();
+        WARDatabase = mongoClient.getDatabase("WARGame");
+    }
 }
