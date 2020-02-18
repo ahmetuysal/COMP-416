c7a2f163f0e927998b343776f960689766a93c8f
Ahmet Uysal <auysal16@ku.edu.tr>
2020-02-18 17:52:57 +0300

    Add correct MongoDB driver to server project :)

--- Project1/Server/src/repository/MongoDBWARRepository.java
+++ Project1/Server/src/repository/MongoDBWARRepository.java
@@ -1,5 +1,13 @@
 package repository;
 
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ConnectionString;
+import com.mongodb.ServerAddress;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoClientOptions;
+
 /**
  * @author Ahmet Uysal @ahmetuysal, Ipek Koprululu @ikoprululu, Furkan Sahbaz @fsahbaz
  */
