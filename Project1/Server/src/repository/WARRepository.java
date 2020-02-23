7312f9e067f508d2f729a02336afb96ec95b76d5
Furkan Sahbaz <fsahbaz16@ku.edu.tr>
2020-02-23 18:01:46 +0300

    JavaDoc information for the included classes.

--- Project1/Server/src/repository/WARRepository.java
+++ Project1/Server/src/repository/WARRepository.java
@@ -6,11 +6,31 @@ import domain.WARGame;
  * @author Ahmet Uysal @ahmetuysal, Ipek Koprululu @ikoprululu, Furkan Sahbaz @fsahbaz
  */
 public interface WARRepository {
+    /**
+     * Inserts the give gameData to the database.
+     *
+     * @param gameData Game data to be inserted into the database.
+     */
     void insertGame(WARGame gameData);
 
+    /**
+     * Obtains the game data with the given ID from the database.
+     *
+     * @param objID ID of the game to be retrieved from the database.
+     */
     void retrieveGame(String objID);
 
+    /**
+     * Updates the given gameData in the database.
+     *
+     * @param gameData Game data to be updated in the database.
+     */
     void updateGame(WARGame gameData);
 
+    /**
+     * Deletes the given game data from the server.
+     *
+     * @param gameData Game data to be deleted from the database.
+     */
     void deleteGame(WARGame gameData);
 }
