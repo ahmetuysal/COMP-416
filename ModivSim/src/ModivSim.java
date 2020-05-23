a6e77de21dd24ab90e047688e2cebc82189d044b
Ahmet Uysal <auysal16@ku.edu.tr>
2020-05-23 15:43:37 +0300

    Start implementing nodeText file reading

--- ModivSim/src/ModivSim.java
+++ ModivSim/src/ModivSim.java
@@ -1,8 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 /**
  * @author Ahmet Uysal @ahmetuysal
  */
 public class ModivSim {
     public static void main(String[] args) {
+
+        try (Stream<Path> walk = Files.walk(Paths.get("src/nodeData"))) {
+            walk.filter(Files::isRegularFile).forEach(path -> {
+                try {
+                    BufferedReader reader = new BufferedReader(new FileReader(path.toFile()));
+                    // TODO: read file content
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            });
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
