fe098f69563626a8f85880e441acfdb45fc71c3f
Ahmet Uysal <auysal16@ku.edu.tr>
2020-05-23 19:06:05 +0300

    Try running simulation

--- ModivSim/src/domain/Node.java
+++ ModivSim/src/domain/Node.java
@@ -1,11 +1,16 @@
 package domain;
 
+import main.ModivSim;
+
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Node extends Thread {
@@ -13,13 +18,13 @@ public class Node extends Thread {
     private final int nodeId;
     private final Map<Integer, Integer> linkCost;
     private final Map<Integer, Integer> linkBandwidth;
-    private final Map<Integer, Integer> distanceVector;
+    private final HashMap<Integer, Integer> distanceVector;
     private final Map<Integer, KeyValuePair<Integer, Integer>> distanceTable;
     private final List<Integer> neighbors;
     private final Map<Integer, Integer> bottleneckBandwidthTable;
     protected ObjectInputStream objectInputStream;
     protected ObjectOutputStream objectOutputStream;
-    private boolean isUpdateRequired = false;
+    private boolean isUpdateRequired = true;
 
     public Node(int nodeId, Map<Integer, Integer> linkCost, Map<Integer, Integer> linkBandwidth) {
         this.nodeId = nodeId;
@@ -38,14 +43,8 @@ public class Node extends Thread {
     }
 
     public void run() {
-        while (true) {
-            sendUpdate();
-            try {
-                wait(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(this::sendUpdate, 0, 200, TimeUnit.MILLISECONDS);
     }
 
 
@@ -69,6 +68,7 @@ public class Node extends Thread {
 
         if (isTableUpdated.get()) {
             this.isUpdateRequired = true;
+            System.out.println("Node " + this.nodeId + " updated its table!");
         }
     }
 
@@ -76,6 +76,14 @@ public class Node extends Thread {
         if (!this.isUpdateRequired) {
             return false;
         }
+
+        this.neighbors.forEach(neighborId -> {
+            try {
+                ModivSim.concurrentMessageQueue.put(new Message(this.nodeId, neighborId, this.linkBandwidth.get(neighborId), new HashMap<>(this.distanceVector)));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 
         return true;
     }
--- ModivSim/src/main/ModivSim.java
+++ ModivSim/src/main/ModivSim.java
@@ -1,24 +1,36 @@
+package main;
+
+import domain.Message;
+import domain.Node;
+
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.stream.Stream;
 
 /**
  * @author Ahmet Uysal @ahmetuysal
  */
 public class ModivSim {
+
+    public static final LinkedBlockingQueue<Message> concurrentMessageQueue = new LinkedBlockingQueue<>();
+
     public static void main(String[] args) {
+
+        HashMap<Integer, Node> nodes = new HashMap<>();
 
         try (Stream<Path> walk = Files.walk(Paths.get("src/nodeData"))) {
             walk.filter(Files::isRegularFile).forEach(path -> {
                 try {
                     BufferedReader reader = new BufferedReader(new FileReader(path.toFile()));
-                    // TODO: read file content
                     String content;
                     while ((content = reader.readLine()) != null) {
                         List<String> l = new ArrayList<>(Arrays.asList(content.split(",")));
@@ -27,33 +39,36 @@ public class ModivSim {
                         HashMap<Integer, Integer> linkCost = new HashMap<>();
                         HashMap<Integer, Integer> linkBandwidth = new HashMap<>();
                         l.remove(0);
-                        System.out.println("Node: " + nodeID);
                         for (String s : l) {
-                            if(s.contains("("))
-                            {
-                                neighborID  = Integer.parseInt(s.substring(s.indexOf("(") + 1));
-                                System.out.println("Neighbor: " + neighborID);
-                            }
-                            else if(s.contains(")"))
-                            {
+                            if (s.contains("(")) {
+                                neighborID = Integer.parseInt(s.substring(s.indexOf("(") + 1));
+                            } else if (s.contains(")")) {
                                 linkBandwidth.put(neighborID, Integer.parseInt(s.substring(0, s.indexOf(")"))));
-                                System.out.println("BW: " + linkBandwidth.get(neighborID));
-                            }
-                            else
-                            {
+                            } else {
                                 linkCost.put(neighborID, Integer.parseInt(s));
-                                System.out.println("Cost: " + linkCost.get(neighborID));
                             }
                         }
+                        nodes.put(nodeID, new Node(nodeID, linkCost, linkBandwidth));
                     }
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        for (Node node : nodes.values()) {
+            node.start();
+        }
+
+        while (true) {
+            try {
+                Message message = concurrentMessageQueue.take();
+                nodes.get(message.getReceiverId()).receiveUpdate(message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
     }
