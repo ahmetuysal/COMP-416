1ea314d9075116f72b6aaba1ee98ce004da55be6
İpek Köprülülü <ikoprululu16@ku.edu.tr>
2020-05-25 21:11:39 +0300

    assigning a shorter path is implemented

--- ModivSim/src/domain/FlowRouting.java
+++ ModivSim/src/domain/FlowRouting.java
@@ -17,6 +17,8 @@ public class FlowRouting {
     private ConcurrentHashMap<String, List<Integer>> activeFlows = new ConcurrentHashMap<String, List<Integer>>();
     private ConcurrentHashMap<String, ArrayList<Integer>> activeFlowPaths = new ConcurrentHashMap<String, ArrayList<Integer>>();
     private ConcurrentHashMap<String, Long> activeFlowTimes = new ConcurrentHashMap<String, Long>();
+    private ConcurrentHashMap<String, Integer> activeFlowDurations = new ConcurrentHashMap<String, Integer>();
+    private ConcurrentHashMap<String, Integer> activeFlowPathChoice = new ConcurrentHashMap<String, Integer>();
     private HashMap<String, List<Integer>> queue = new HashMap<String, List<Integer>>();
     private Clock clock;
     private int flowsNum;
@@ -68,12 +70,15 @@ public class FlowRouting {
             ArrayList<Integer> bandwidth = new ArrayList<Integer>();
             boolean activated = true;
             path.add(currentNode.getNodeId());
+            int choice = 1;
             while(nodeStep != endNode){
                 boolean linkIsEmpty = false;
                 for(int i = 0; i<2; i++){
                     nodeStep = currentNode.getForwardingTable().get(endNode).get(i);
                     if(links[currentNode.getNodeId()][nodeStep] == false){
                         linkIsEmpty = true;
+                        if(i == 1)
+                            choice = 2;
                         break;
                     }
                 }
@@ -84,6 +89,7 @@ public class FlowRouting {
                 }else{
                     activated = false;
                     queue.put(flowLabel, contents);
+                    System.out.println("Queue is updated: " + queue);
                     break;
                 }
             }
@@ -92,21 +98,30 @@ public class FlowRouting {
                 for(int i=0; i<path.size()-1; i++){
                     links[path.get(i)][path.get(i+1)] = true;
                 }
-                System.out.println(path + " " + bandwidth);
+                System.out.println("Path for " + flowLabel + ": " + path);
                 int min = 999;
                 for(int i=0; i<bandwidth.size(); i++){
                     if(bandwidth.get(i) < min)
                         min = bandwidth.get(i);
                 }
-                int time = contents.get(2) / min;
-                long endTime = clock.millis() + time * 1000;
+                int time = (contents.get(2) / min) * 1000;
+                long endTime = clock.millis() + time;
                 activeFlowTimes.put(flowLabel, endTime);
+                activeFlowDurations.put(flowLabel, time);
                 activeFlowPaths.put(flowLabel, path);
                 activeFlows.put(flowLabel, contents);
+                activeFlowPathChoice.put(flowLabel, choice);
             }
 
-            if(checkActiveFlows())
+            if(checkActiveFlows()){
                 checkQueue();
+                for(String fl : activeFlowPathChoice.keySet()){
+                    if (activeFlowPathChoice.get(fl) == 2)
+                        checkFirstChoice(fl);
+                }
+            }
+
+
         }
 
         while(!activeFlows.isEmpty() || !queue.isEmpty()){
@@ -114,27 +129,81 @@ public class FlowRouting {
                 checkActiveFlows();
             if(queue.size() != 0)
                 checkQueue();
+            for(String fl : activeFlowPathChoice.keySet()){
+                if (activeFlowPathChoice.get(fl) == 2)
+                    checkFirstChoice(fl);
+            }
         }
     }
 
     private boolean checkActiveFlows(){
         boolean modified = false;
         for(String flowLabel : activeFlows.keySet()){
+            ArrayList<Integer> currentPath = activeFlowPaths.get(flowLabel);
             if(clock.millis() > activeFlowTimes.get(flowLabel)){
-                ArrayList<Integer> path = activeFlowPaths.get(flowLabel);
-                for(int i=0; i< path.size()-1; i++){
-                    links[path.get(i)][path.get(i+1)] = true;
+                for(int i=0; i< currentPath.size()-1; i++){
+                    links[currentPath.get(i)][currentPath.get(i+1)] = false;
                 }
                 activeFlows.remove(flowLabel);
                 activeFlowTimes.remove(flowLabel);
                 activeFlowPaths.remove(flowLabel);
-                System.out.println("Active flow count: " + activeFlows.size());
+                activeFlowPathChoice.remove(flowLabel);
+                activeFlowDurations.remove(flowLabel);
+                System.out.println("Flow " + flowLabel + " is terminated.");
                 modified = true;
             }
         }
         return modified;
     }
 
+    private void checkFirstChoice(String flowLabel){
+        ArrayList<Integer> currentPath = activeFlowPaths.get(flowLabel);
+        List<Integer> contents = flows.get(flowLabel);
+        Node currentNode = nodes.get(contents.get(0));
+        int endNode = contents.get(1);
+        int nodeStep = -1;
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        ArrayList<Integer> bandwidth = new ArrayList<Integer>();
+        boolean activated = true;
+        path.add(currentNode.getNodeId());
+        while(nodeStep != endNode){
+            boolean linkIsEmpty = false;
+            nodeStep = currentNode.getForwardingTable().get(endNode).get(0);
+            if(links[currentNode.getNodeId()][nodeStep] == false){
+                bandwidth.add(currentNode.getLinkBandwidth().get(nodeStep));
+                currentNode = nodes.get(nodeStep);
+                path.add(nodeStep);
+            }else{
+                activated = false;
+                break;
+            }
+        }
+        if(activated) {
+            for(int i=0; i< currentPath.size()-1; i++){
+                links[currentPath.get(i)][currentPath.get(i+1)] = false;
+            }
+
+            for(int i=0; i<path.size()-1; i++){
+                links[path.get(i)][path.get(i+1)] = true;
+            }
+
+            System.out.println("Updated path for " + flowLabel + ": " + path);
+            int min = 999;
+            for(int i=0; i<bandwidth.size(); i++){
+                if(bandwidth.get(i) < min)
+                    min = bandwidth.get(i);
+            }
+
+            long flowTime = activeFlowTimes.get(flowLabel);
+            int duration = activeFlowDurations.get(flowLabel);
+
+            int time = (contents.get(2)*((int) (flowTime-clock.millis())/duration)) / min;
+            //System.out.println("time: " + time);
+            long endTime = clock.millis() + time * 1000;
+            activeFlowPaths.put(flowLabel, path);
+            activeFlowTimes.put(flowLabel, endTime);
+        }
+    }
     private void checkQueue(){
         for(String flowLabel : queue.keySet()) {
             List<Integer> contents = queue.get(flowLabel);
@@ -168,7 +237,7 @@ public class FlowRouting {
                 for (int i = 0; i < path.size() - 1; i++) {
                     links[path.get(i)][path.get(i + 1)] = true;
                 }
-                System.out.println(path + " " + bandwidth);
+                System.out.println("Path for " + flowLabel + ": " + path);
                 int min = 999;
                 for(int i=0; i<bandwidth.size(); i++){
                     if(bandwidth.get(i) < min)
@@ -180,6 +249,7 @@ public class FlowRouting {
                 activeFlowPaths.put(flowLabel, path);
                 activeFlows.put(flowLabel, contents);
                 queue.remove(flowLabel);
+                System.out.println("Queue is updated: " + queue);
             }
         }
     }
